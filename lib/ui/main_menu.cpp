4a762575aef9c302b9f8fad234889320e48cdfcc
Behnam Khanbeigi <b.khanbeigi@gresb.com>
2024-03-04 16:40:58 +0100

    Add main menu and fix touch issues

--- lib/ui/main_menu.cpp
+++ lib/ui/main_menu.cpp
@@ -1,11 +1,3 @@
-/* void render_main_menu(){ */
-/*     lv_obj_t * btn = lv_btn_create(lv_scr_act()); */
-/*     lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_PRESSED, NULL);  */
-/*     lv_obj_align( btn, LV_ALIGN_CENTER, 0, 0 ); */
-/*           lv_obj_t * label; */
-/*     label = lv_label_create(btn); */
-/*     lv_label_set_text(label, "Hell Yeah"); */
-/* } */
 # include "Arduino.h"
 # include "main_menu.h"
 #include <lvgl.h>
@@ -17,54 +9,68 @@ MainMenu::MainMenu(){
 
 void MainMenu::eventHandler(lv_event_t * event){
   Serial.println("lv_event_get_target(event)");
+
+  // if event is  set mode to SEQ
+
+  // if (
+  //   globalMode = SEQ;
+  // }else{
+  //      globalMode = SYNTH;
+  // }
 }
 
 void MainMenu::render(){
-  lv_obj_t * btn = lv_btn_create(lv_scr_act());
-  lv_obj_add_event_cb(btn, eventHandler, LV_EVENT_PRESSED, NULL);
-  lv_obj_align( btn, LV_ALIGN_CENTER, 0, 0 );
-  lv_obj_t * label;
-  label = lv_label_create(btn);
-  lv_label_set_text(label, "Hell Yeah");
+    /*Create a menu object*/
+    lv_obj_t * menu = lv_menu_create(lv_scr_act());
+    lv_obj_set_size(menu, lv_disp_get_hor_res(NULL), lv_disp_get_ver_res(NULL));
+    lv_obj_center(menu);
+
+    lv_obj_t * cont;
+    lv_obj_t * label;
 
 
-    // lv_obj_t * menu = lv_menu_create(lv_scr_act());
-    // lv_obj_set_size(menu, lv_disp_get_hor_res(NULL), lv_disp_get_ver_res(NULL));
-    // lv_obj_center(menu);
+    lv_obj_t * synth_page = lv_menu_page_create(menu, NULL);
 
-    // lv_obj_t * cont;
-    // lv_obj_t * label;
+    cont = lv_menu_cont_create(synth_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "SYNTH PAGE");
 
-    // /*Create a sub page*/
-    // lv_obj_t * sub_page = lv_menu_page_create(menu, NULL);
-    // lv_menu_set_mode_root_back_button(sub_page, LV_MENU_ROOT_BACK_BUTTON_ENABLED);
+    lv_obj_t * arpeggiator_page = lv_menu_page_create(menu, NULL);
+
+    cont = lv_menu_cont_create(arpeggiator_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "ARPEGGIATOR PAGE");
+
+    lv_obj_t * sequencer_page = lv_menu_page_create(menu, NULL);
+
+    cont = lv_menu_cont_create(sequencer_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "SEQUENCER PAGE");
     
-    // cont = lv_menu_cont_create(sub_page);
-    // label = lv_label_create(cont);
-    // lv_label_set_text(label, "Synth Mode");
 
-    // /*Create a main page*/
-    // lv_obj_t * main_page = lv_menu_page_create(menu, NULL);
-    // //lv_obj_add_event_cb(main_page, eventHandler, LV_EVENT_CLICKED, main_page);
+    /*Create a main page*/
+    lv_obj_t * main_page = lv_menu_page_create(menu, NULL);
 
-    // cont = lv_menu_cont_create(main_page);
-    // label = lv_label_create(cont);
-    // lv_label_set_text(label, "Synth Mode");
-    // //lv_menu_set_load_page_event(menu, cont, sub_page);
+    cont = lv_menu_cont_create(main_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "SYNTHESIZER");
+    lv_menu_set_load_page_event(menu, cont, synth_page);
+    lv_obj_add_event_cb(cont, eventHandler, LV_EVENT_PRESSED, NULL);
 
-    // cont = lv_menu_cont_create(main_page);
-    // label = lv_label_create(cont);
-    // lv_label_set_text(label, "Sequencer Mode");
+    cont = lv_menu_cont_create(main_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "ARPEGGIATOR");
+    lv_menu_set_load_page_event(menu, cont, arpeggiator_page);
+    lv_obj_add_event_cb(cont, eventHandler, LV_EVENT_PRESSED, NULL);
 
-    // cont = lv_menu_cont_create(main_page);
-    // label = lv_label_create(cont);
-    // lv_label_set_text(label, "Sampler Mode");
-    // lv_menu_set_load_page_event(menu, cont, sub_page);
+    cont = lv_menu_cont_create(main_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "SEQUENCER");
+    lv_menu_set_load_page_event(menu, cont, sequencer_page);
+    lv_obj_add_event_cb(cont, eventHandler, LV_EVENT_PRESSED, NULL);
 
-    // lv_menu_set_page(menu, main_page);
+    lv_menu_set_page(menu, main_page);
      
 }
-  
-
 
 MainMenu main_menu = MainMenu();
--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,6 @@
 #include <TFT_eSPI.h>
 //#endif
 
-#include "main_menu.h"
-
-// FROM TFT_eSPI LIB
-//#define ILI9341_DRIVER
-//#define ILI9341_DRIVER 
-//#define TFT_MISO  12  // Automatically assigned with ESP8266 if not defined
-//#define TFT_MOSI  11  // Automatically assigned with ESP8266 if not defined
-//#define TFT_SCLK  13  // Automatically assigned with ESP8266 if not defined
-//#define TFT_CS    3  // Chip select control pin D8
-//#define TFT_DC    2   // Data Command control pin
-//#define TFT_RST   -1  // Reset pin (could connect to NodeMCU RST, see next line)
-//#define TOUCH_CS 5     // Chip select pin (T_CS) of touch screen
-
 // XPT2046_Touchscreen
 #define CS_PIN  5
 #define TIRQ_PIN  4
@@ -27,12 +14,17 @@
 #define TFT_HOR_RES   320
 #define TFT_VER_RES   240
 
+
 XPT2046_Touchscreen ts(CS_PIN, TIRQ_PIN);
 TFT_eSPI tft = TFT_eSPI(TFT_HOR_RES, TFT_VER_RES); 
 
 /*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
 #define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
 uint32_t draw_buf[DRAW_BUF_SIZE / 4];
+
+
+#include "main_menu.h"
+
 
 #if LV_USE_LOG != 0
 void print_logs( lv_log_level_t level, const char * buf )
@@ -57,36 +49,37 @@ void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
   lv_disp_flush_ready(disp);
 }
 
-/*Read the touchpad*/
-void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
+
+
+
+void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data ) 
 {
-    bool touched = ts.touched();
-    TS_Point p = ts.getPoint();
-    int x = map(p.x, 220, 3850, 1, TFT_HOR_RES);
-    int y = map(p.y, 310, 3773, 1, TFT_VER_RES); 
+  bool touched = ts.touched();
+  TS_Point p = ts.getPoint();
 
-    if(!touched) {
-        data->state = LV_INDEV_STATE_RELEASED;
-    } else {
-        data->state = LV_INDEV_STATE_PRESSED;
+  int x = map(p.y, 400, 3829, 1, TFT_VER_RES);
+  int y = map(p.x, 540, 3756, 1, TFT_HOR_RES);
+  if(!touched) {    
+    data->state = LV_INDEV_STATE_RELEASED;
+  } else {
+    data->state = LV_INDEV_STATE_PRESSED;
 
-        data->point.x = x;
-        data->point.y = y;
-    }
+    data->point.x = x;
+    data->point.y = y;
+  }
 }
- void btn_event_cb(lv_event_t * event){
-     Serial.println("Clicked\n");
- }
 
 void setup()
 {
     Serial.begin( 115200 );
 
     lv_init();
-    ts.begin();
     tft.begin(); /* TFT init */
-    tft.setRotation(1); /* Landscape orientation */
-
+    tft.setRotation(3); /* Landscape orientation */
+    ts.begin();
+    //tft.setRotation(0);
+    ts.setRotation(0);
+    
     /* register print function for debugging */
     #if LV_USE_LOG != 0
        lv_log_register_print_cb( print_logs );
@@ -105,7 +98,7 @@ void setup()
     lv_indev_t * indev = lv_indev_create();
     lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
     lv_indev_set_read_cb(indev, my_touchpad_read);
-
+ 
     main_menu.render();
     		// lv_obj_t * btn = lv_btn_create(lv_scr_act());
 		// lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_PRESSED, NULL); 
@@ -121,4 +114,5 @@ void loop()
 {
     lv_task_handler(); /* let the GUI do its work */
     lv_tick_inc(5); /* tell LVGL how much time has passed */
+
 }
