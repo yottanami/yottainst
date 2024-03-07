cd0e41954d1f1c91cdef5e05163541b55e9a239c
Behnam Khanbeigi <b.khanbeigi@gresb.com>
2024-03-07 21:49:24 +0100

    Add envelope

--- lib/synthesizer/synthesizer.cpp
+++ lib/synthesizer/synthesizer.cpp
@@ -12,18 +12,21 @@ AudioSynthWaveform       waveform2;      //xy=258,335
 AudioSynthWaveform       waveform1;      //xy=260,272
 AudioSynthNoisePink      pink1;          //xy=265,391
 AudioMixer4              mixer1;         //xy=464,372
-AudioOutputI2S           i2s1;           //xy=637,371
+AudioEffectEnvelope      envelope1;      //xy=615,372
+AudioOutputI2S           i2s1;           //xy=806,381
 AudioConnection          patchCord1(waveform2, 0, mixer1, 1);
 AudioConnection          patchCord2(waveform1, 0, mixer1, 0);
 AudioConnection          patchCord3(pink1, 0, mixer1, 2);
-AudioConnection          patchCord4(mixer1, 0, i2s1, 0);
-AudioConnection          patchCord5(mixer1, 0, i2s1, 1);
+AudioConnection          patchCord4(mixer1, envelope1);
+AudioConnection          patchCord5(envelope1, 0, i2s1, 0);
+AudioConnection          patchCord6(envelope1, 0, i2s1, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=448,517
 // GUItool: end automatically generated code
 
+
 // GLOBAL VARIABLES 
 const byte BUFFER = 8; //Size of keyboard buffer
-const float noteFreqs[128] = {8.176, 8.662, 9.177, 9.723, 10.301, 10.913, 11.562, 12.25, 12.978, 13.75, 14.568, 15.434, 16.352, 17.324, 18.354, 19.445, 20.602, 21.827, 23.125, 24.5, 25.957, 27.5, 29.135, 30.868, 32.703, 34.648, 36.708, 38.891, 41.203, 43.654, 46.249, 48.999, 51.913, 55, 58.27, 61.735, 65.406, 69.296, 73.416, 77.782, 82.407, 87.307, 92.499, 97.999, 103.826, 110, 116.541, 123.471, 130.813, 138.591, 146.832, 155.563, 164.814, 174.614, 184.997, 195.998, 207.652, 220, 233.082, 246.942, 261.626, 277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305, 440, 466.164, 493.883, 523.251, 554.365, 587.33, 622.254, 659.255, 698.456, 739.989, 783.991, 830.609, 880, 932.328, 987.767, 1046.502, 1108.731, 1174.659, 1244.508, 1318.51, 1396.913, 1479.978, 1567.982, 1661.219, 1760, 1864.655, 1975.533, 2093.005, 2217.461, 2349.318, 2489.016, 2637.02, 2793.826, 2959.955, 3135.963, 3322.438, 3520, 3729.31, 3951.066, 4186.009, 4434.922, 4698.636, 4978.032, 5274.041, 5587.652, 5919.911, 6271.927, 6644.875, 7040, 7458.62, 7902.133, 8372.018, 8869.844, 9397.273, 9956.063, 10548.08, 11175.3, 11839.82, 12543.85};
+const float noteFreqs[128] = {8.176, 8.662, 9.177, 9.723, 10.301, 10.913, 11.562, 12.25, 12.978, 13.75, 14.568, 15.434, 16.352, 17.324, 18.354, 19.445, 20.602, 21.827, 23.125, 24.5, 25.957, 27.5, 29.135, 30.868, 32.703, 34.648, 36.708, 38.891, 41.203, 43.654, 46.249, 48.999, 51.913, 55, 58.27, 61.735, 65.406, 69.296, 73.416, 77.782, 82.407, 87.307, 92.499, 97.999, 103.826, 110, 116.541, 123.471, 130.813, 138.591, 146.832, 155.563, 164.814, 174.614, 184.997, 195.998, 207.652, 220, 233.082, 246.942, 261.626, 277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305, 440, 466.164, 493.883, 523.251, 554.365, 587.33, 622.254, 659.255, 698.456, 739.989, 783.991, 830.609, 880, 932.328, 987.767, 1046.502, 1108.731, 1174.659, 1244.508, 1318.51, 1396.913, 1479.978, 1567.982, 1661.219, 1760, 1864.655, 1975.533, 2093.005, 2217.461, 2349.318, 2489.016, 2637.02, 2793.826, 2959.955, 3135.963, 3322.438, 3520, 3729.31, 3951.066, 4186.009, 4434.922, 4698.636, 4978.032, 5274.041, 5587.652, 5919.911,  6271.927, 6644.875, 7040, 7458.62, 7902.133, 8372.018, 8869.844, 9397.273, 9956.063, 10548.08, 11175.3, 11839.82, 12543.85};
 
 byte globalNote = 0;
 byte globalVelocity = 0;
@@ -57,6 +60,11 @@ void Synthesizer::setup(){
   mixer1.gain(0, 1.0);
   mixer1.gain(1, 1.0);
   mixer1.gain(2, 1.0);
+
+  envelope1.attack(0);
+  envelope1.decay(0);
+  envelope1.sustain(1);
+  envelope1.release(500);
 }
 
 void Synthesizer::onControlChange(byte channel, byte control, byte value){
@@ -93,6 +101,22 @@ void Synthesizer::onControlChange(byte channel, byte control, byte value){
       Synthesizer::octave = -24;
       break;
     }
+    break;
+
+  case 104:
+    envelope1.attack(3000 * (value * DIV127));
+    break;
+
+  case 105:
+    envelope1.decay(3000 * (value * DIV127));
+    break;
+
+  case 106:
+    envelope1.sustain(value * DIV127);
+    break;
+
+  case 107:
+    envelope1.release(3000 * (value * DIV127));
     break;
   }
 }
@@ -151,12 +175,12 @@ void Synthesizer::oscPlay(byte note) {
   waveform1.amplitude(velo);
   waveform2.amplitude(velo);
   pink1.amplitude(velo);
+
+  envelope1.noteOn();
 }
 
 void Synthesizer::oscStop() {
-  waveform1.amplitude(0.0);
-  waveform2.amplitude(0.0);
-  pink1.amplitude(0.0);
+  envelope1.noteOff();
 }    
 
 Synthesizer synthesizer = Synthesizer();
